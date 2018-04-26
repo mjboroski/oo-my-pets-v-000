f8266632f71f865b429fd40db68467627278531e
Michael Boroski <mjboroski@gmail.com>
2018-04-26 03:12:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,15 +43,15 @@ class Owner
   end
 
   def buy_fish(name)
-    @pets[fishes]<<Fish.new(name)
+    pets[:fishes]<<Fish.new(name)
   end
 
   def buy_cat(name)
-    @cats<<Cat.new(name)
+    pets[:cats]<<Cat.new(name)
   end
 
   def buy_dog(name)
-    @dogs<<Dog.new(name)
+    pets[:dogs]<<Dog.new(name)
   end
 
   def feed_fish
